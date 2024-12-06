89da16b7f4df3418d71f02edf0e9038e2bf2b86d
NirajAnupSingh <nirajanupsingh2006@gmail.com>
2024-12-06 13:59:49 +0530

    A2Q4 modified

---
+++ pps_assignment/A2Q4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+    printf("The ASCII value of %c is %d",ch,ch);
+    if ((ch>=65 && ch<=90)||(ch>='a' && ch<='z'))
+    {
+        printf("\nThe character is an alphabet");
+    }
+    else if (ch>=48 && ch<=57)
+    {
+        printf("\nThe character is a digit");
+        if (ch%2==0)
+        {
+            printf("\nThe digit is even");
+        }
+        else
+        {
+            printf("\nThe digit is odd");
+        }
+    }
+    else
+    {
+            printf("\nThe character is a special symbol");
+    }
+return 0;
+}
